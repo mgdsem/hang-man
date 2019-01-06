15db2128614d3ef524cba2a669da5745a9a9b18a
Magdalena Semik <magda@MacBook-Air-Magdalena.local>
2019-01-06 22:18:50 +0100

    add new element - letters

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("connected");
+// constans
+
+const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
+console.log(LETTERS);
+
+// HTML elements
+
+const lettersContainer = document.getElementById('letters-container');
+lettersContainer.classList.add('letters-container');
+
+// logic
+
+function render() {
+    LETTERS.forEach((letter) => {
+        console.log(letter);
+        const letterContainer = document.createElement('div');
+        letterContainer.innerText = letter;
+        letterContainer.classList.add('letter-container');
+        lettersContainer.appendChild(letterContainer);
+    })
+}
+
+render();
