6cab4c5aa0dbd76ac5b21be4741d4037bd22a115
Magdalena Semik <magda@MacBook-Air-Magdalena.local>
2019-01-08 23:18:59 +0100

    make basics of header

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // constans
 
 const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
-console.log(LETTERS);
+const SENTENCE = 'Nie chwal shakiego przed zjebaniem jego'
 
 // HTML elements
 
 const lettersContainer = document.getElementById('letters-container');
+const header = document.getElementById('header');
 
 // logic
 
 function render() {
     LETTERS.forEach((letter) => {
-        console.log(letter);
         const letterContainer = document.createElement('div');
         letterContainer.classList.add('letter-container');
         const letterButton = document.createElement('button');
@@ -20,6 +20,33 @@ function render() {
         letterContainer.appendChild(letterButton);
         lettersContainer.appendChild(letterContainer);
     })
+
+    header.innerText = generateInitialSigns();
 }
+
+function generateInitialSigns() {
+    // const signs = SENTENCE.split('')
+    // const signsArray = signs.map((sign) => {
+    //     if (sign !== ' ') {
+    //         return '-';
+    //     }
+    //     return ' ';
+    // })
+
+    // const signsArray = signs.map((sign)=> {
+    //     return sign !== ' ' ? '-' : ' '
+    // })
+
+    // const signsArray = signs.map(sign => sign !== ' ' ? '-' : ' ');
+    // const finalSigns = signsArray.join('');
+    // console.log(finalSigns);
+
+    // const signs = SENTENCE.split('').map(sign => sign !== ' ' ? '-' : ' ').join('');
+    // console.log(signs);
+    // return signs;
+
+    return SENTENCE.split('').map(sign => sign !== ' ' ? '-' : ' ').join('');
+}
+
 
 render();
