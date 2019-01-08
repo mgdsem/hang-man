69585ffea15f04cb550cb90f7124a440067e1724
Magdalena Semik <magda@MacBook-Air-Magdalena.local>
2019-01-08 23:34:15 +0100

    make cleanings

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 // constans
 
-const LETTERS = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const LETTERS = 'aąbcdęfghijklłmnoópqrsśtuvwxyz'.split('');
 const SENTENCE = 'Nie chwal shakiego przed zjebaniem jego'
+
+const state = {
+    currentAnswer: generateInitialSigns(),
+}
 
 // HTML elements
 
 const lettersContainer = document.getElementById('letters-container');
 const header = document.getElementById('header');
 
-// logic
+// event listeners
 
-function render() {
-    LETTERS.forEach((letter) => {
-        const letterContainer = document.createElement('div');
-        letterContainer.classList.add('letter-container');
-        const letterButton = document.createElement('button');
-        letterButton.innerText = letter;
-        letterButton.classList.add('letter-button');
-        letterContainer.appendChild(letterButton);
-        lettersContainer.appendChild(letterContainer);
-    })
+function onPickLetter() {
 
-    header.innerText = generateInitialSigns();
 }
+// sprawdz czy wybrana litera znajduje sie w zdaniu do odgadniecia
+// jesli tak, to odkryj ja w headerze i zaznacz przycisk na zielono i go zablokuj
+// jesli nie, to zaznacz przycisk na czerwono i go zablokuj i pokaz kolejne zdjecie
+
+// logic
 
 function generateInitialSigns() {
     // const signs = SENTENCE.split('')
@@ -48,5 +47,23 @@ function generateInitialSigns() {
     return SENTENCE.split('').map(sign => sign !== ' ' ? '-' : ' ').join('');
 }
 
+// renders
+
+function render() {
+    renderLetters();
+    header.innerText = state.currentAnswer;
+}
+
+function renderLetters() {
+    LETTERS.forEach((letter) => {
+        const letterContainer = document.createElement('div');
+        letterContainer.classList.add('letter-container');
+        const letterButton = document.createElement('button');
+        letterButton.innerText = letter;
+        letterButton.classList.add('letter-button');
+        letterContainer.appendChild(letterButton);
+        lettersContainer.appendChild(letterContainer);
+    })
+}
 
 render();
