cc673030ae9c21e27470a2e6b8fea41cfb6c8294
Magdalena Semik <magda@MacBook-Air-Magdalena.local>
2019-01-10 23:18:23 +0100

    create mechanism for picked letters

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const LETTERS = 'aąbcdęfghijklłmnoópqrsśtuvwxyz'.split('');
 const SENTENCE = 'Nie chwal shakiego przed zjebaniem jego'
 
 const state = {
-    currentAnswer: generateInitialSigns(),
+    currentAnswer: generateAnswer(),
+    correctLetters: [],
 }
 
 // HTML elements
@@ -14,16 +15,35 @@ const header = document.getElementById('header');
 
 // event listeners
 
-function onPickLetter() {
-
+function onPickLetter(e) {
+    const pickedLetter = event.target.innerText;
+    if (SENTENCE.split('').includes(pickedLetter)) {
+        state.currentAnswer = generateAnswer(pickedLetter);
+        state.correctLetters.push(pickedLetter);
+    }
+    render();
 }
 // sprawdz czy wybrana litera znajduje sie w zdaniu do odgadniecia
 // jesli tak, to odkryj ja w headerze i zaznacz przycisk na zielono i go zablokuj
 // jesli nie, to zaznacz przycisk na czerwono i go zablokuj i pokaz kolejne zdjecie
 
+
+
 // logic
 
-function generateInitialSigns() {
+function generateAnswer(pickedLetter) {
+    if (pickedLetter) {
+        return SENTENCE.split('').map(letter => {
+            if (letter === pickedLetter || state.correctLetters.includes(letter)) {
+                return letter;
+            }
+            if (letter === ' ') {
+                return ' ';
+            }
+            return "-";
+        }).join('');
+    }
+
     // const signs = SENTENCE.split('')
     // const signsArray = signs.map((sign) => {
     //     if (sign !== ' ') {
@@ -55,14 +75,17 @@ function render() {
 }
 
 function renderLetters() {
+    lettersContainer.innerHTML = null;
     LETTERS.forEach((letter) => {
         const letterContainer = document.createElement('div');
         letterContainer.classList.add('letter-container');
         const letterButton = document.createElement('button');
         letterButton.innerText = letter;
+        letterButton.addEventListener('click', onPickLetter);
         letterButton.classList.add('letter-button');
         letterContainer.appendChild(letterButton);
         lettersContainer.appendChild(letterContainer);
+
     })
 }
 
